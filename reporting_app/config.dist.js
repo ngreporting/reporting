4925390c494ca0881dbcee8be2cbee5a5d772850
Jan <janvogt@users.noreply.github.com>
2016-11-12 21:35:50 +0100

    Fix security.

--- reporting_app/config.dist.js
+++ reporting_app/config.dist.js
@@ -1,12 +1,14 @@
 const ReportingConfig = {
     firebase: {
-        apiKey: "AIzaSyAJLXGwliG67_agBqpe0KpRQm9wXP5becM",
-        authDomain: "reporting-1bb93.firebaseapp.com",
-        databaseURL: "https://reporting-1bb93.firebaseio.com",
-        storageBucket: "reporting-1bb93.appspot.com",
-        messagingSenderId: "491744898632"
+        // get from firebase.
+        apiKey: "",
+        authDomain: "",
+        databaseURL: "",
+        storageBucket: "",
+        messagingSenderId: ""
     },
-    domain: "reporting-1bb93.firebaseapp.com"
+    // unique url.
+    domain: ""
   };
 
 export default ReportingConfig
